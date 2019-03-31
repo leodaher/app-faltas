5e9533d85f99b01452961d1f4e0c2d27b5550cff
Leonardo Daher <leonardo.daher@usp.br>
2019-03-31 01:12:45 -0300

    Aligning elements inside screen

--- frontend/app/screens/HomeScreen/index.js
+++ frontend/app/screens/HomeScreen/index.js
@@ -5,15 +5,15 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <View style={{ flex: 4, alignItems: "center", justifyContent: "center" }}>
+        <View style={{ flex: 7, alignItems: "center", justifyContent: "center" }}>
           <Text>App de Faltas</Text>
         </View>
         <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
-          <TouchableHighlight style={{ flex:1, backgroundColor: 'blue'}}>
-            <Text>Cadastrar</Text>
+          <TouchableHighlight style={{ flex:1, backgroundColor: '#11122E', justifyContent: "center", alignItems: "center"}}>
+            <Text style={{ color: 'white', fontSize: 20, fontFamily: "monospace"}}>Cadastrar</Text>
           </TouchableHighlight>
-          <TouchableHighlight style={{ flex:1, backgroundColor: 'yellow'}}>
-            <Text>Entrar</Text>
+          <TouchableHighlight style={{ flex:1, backgroundColor: '#F5F8F5', justifyContent: "center", alignItems: "center"}}>
+            <Text style={{ fontSize: 20}}>Entrar</Text>
           </TouchableHighlight>
         </View>
       </View>
